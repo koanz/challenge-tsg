10607d932dcab7a38ed6b2f29053c8fa9a1ddc74
Cristian <cristiananzawa@gmail.com>
2025-04-14 17:28:07 -0300

    naming routes

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,17 @@ use Illuminate\Support\Facades\Route;
 
 // Rutas protegidas con token de acceso JWT
 Route::middleware([JwtTokenAuthenticate::class])->group(function () {
-    Route::get('/users', [UserRestController::class, 'index']);
+    Route::get('/users', [UserRestController::class, 'index'])->name('user-index');
+    Route::post('/users', [UserRestController::class, 'create'])->name('user-create');
+    Route::get('/users/{id}', [UserRestController::class, 'find'])->name('user-find');
+    Route::patch('/users/{id}', [UserRestController::class, 'update'])->name('user-update');
+    Route::delete('/users/{id}', [UserRestController::class, 'delete'])->name('user-delete');
 
-    Route::get('/posts', [PostRestController::class, 'index']);
-    Route::post('/posts', [PostRestController::class, 'create']);
-    Route::get('/posts/{id}', [PostRestController::class, 'find']);
-    Route::patch('/posts/{id}', [PostRestController::class, 'update'])->middleware(VerifyPostOwner::class);
-    Route::delete('/posts/{id}', [PostRestController::class, 'delete'])->middleware(VerifyPostOwner::class);
+    Route::get('/posts', [PostRestController::class, 'index'])->name('post-index');
+    Route::post('/posts', [PostRestController::class, 'create'])->name('post-create');
+    Route::get('/posts/{id}', [PostRestController::class, 'find'])->name('post-find');
+    Route::patch('/posts/{id}', [PostRestController::class, 'update'])->middleware(VerifyPostOwner::class)->name('post-update');
+    Route::delete('/posts/{id}', [PostRestController::class, 'delete'])->middleware(VerifyPostOwner::class)->name('post-delete');
 });
 
 // Rutas públicas sin verificación de Token JWT

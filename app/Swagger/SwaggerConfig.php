78d056a211da2f9a54717c4830d73f3766c1cd17
Cristian <cristiananzawa@gmail.com>
2025-04-14 17:34:42 -0300

    swagger config and doc annotations

---
+++ app/Swagger/SwaggerConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @OA\Tag(
+ *     name="Usuarios",
+ *     description="Endpoints para la gestión de usuarios"
+ * )
+ */
+/**
+ * @OA\Tag(
+ *     name="Posts",
+ *     description="Endpoints para la gestión de posts"
+ * )
+ */
+/**
+ * @OA\Tag(
+ *     name="Autenticación",
+ *     description="Endpoints para el registro e inicio de sesión"
+ * )
+ */
+

4bbf569eb016a2461b7bb30bb46fa6b6c3635b90
Cristian <cristiananzawa@gmail.com>
2025-04-15 18:18:58 -0300

    validation error response for all request

--- app/Http/Requests/BaseFormRequest.php
+++ app/Http/Requests/BaseFormRequest.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class BaseFormRequest extends FormRequest
 {
+    /**
+     * Respuesta de error en las validaciones.
+     */
     protected function failedValidation(Validator|\Illuminate\Contracts\Validation\Validator $validator)
     {
         throw new HttpResponseException(response()->json([
             'status' => 'validation_error',
             'errors' => $validator->errors(),
-        ], 422));
+        ], 400));
     }
 }

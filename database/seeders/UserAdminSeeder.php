e61c5a9361854d4f38e89c9159c20274275a6fab
Cristian <cristiananzawa@gmail.com>
2025-04-15 12:02:49 -0300

    update migrations and seeders

--- database/seeders/UserAdminSeeder.php
+++ database/seeders/UserAdminSeeder.php
@@ -6,19 +6,20 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
-class UserSeeder extends Seeder
+class UserAdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        // Creación de usuario administrador
+        // Creación de usuario con asignación de rol administrador
         $admin = User::create([
             'name' => 'Admin',
             'email' => 'admin@email.com',
-            'password' => Hash::make('challenge'),
-        ]);
+            'password' => Hash::make('123456'),
+        ])->assignRole('admin');
     }
 }
